94a0de9978210387f0c52c6c84121d5668f3d63b
Aaron Clark <amclarkwustl@gmail.com>
2023-08-05 12:53:51 -0700

    init setup

---
+++ src/main/java/com/fm/FridgeMates/FridgeMatesApplication.java
@@ -0,0 +1,13 @@
+package com.fm.FridgeMates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeMatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeMatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fm/FridgeMates/FridgeMatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fm.FridgeMates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeMatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
